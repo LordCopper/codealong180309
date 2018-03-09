8e7864773fdbc00b02254fb682f3485d5a27a8bc
John Hellman <acoppero@gmail.com>
2018-03-09 15:58:12 +0100

    bopp

--- CodeAlong180309/Program.cs
+++ CodeAlong180309/Program.cs
@@ -17,6 +17,7 @@ namespace CodeAlong180309
                 Console.WriteLine("Please make your chooses by inputing the corresponding number");
                 Console.WriteLine("1) Cinema");
                 Console.WriteLine("2) Repeat");
+                Console.WriteLine("3) Third Word");
                 Console.WriteLine("0) Quit");
                 string input = Console.ReadLine();
 
@@ -27,6 +28,9 @@ namespace CodeAlong180309
                         break;
                     case "2":
                         Repeat();
+                        break;
+                    case "3":
+                        ThirdWord();
                         break;
                     case "0":
                         keepRunningProgram = false;
@@ -40,26 +44,67 @@ namespace CodeAlong180309
 
         public static void Cinema()
         {
-            Console.WriteLine("Please enter the age of the movie goer (numericaly):");
-            string input = Console.ReadLine();
-            int movieGoerAge = int.Parse(input);
-            if(movieGoerAge<20 || movieGoerAge > 64 )
+            int movieGoerAge, groupSize, groupPrice = 0;
+            string input;
+
+            do
             {
-                if(movieGoerAge<20)
+                Console.WriteLine("How many will be attending the cinema:");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out groupSize));
+
+            
+
+            for (int i = 0; i < groupSize; i++)
+            {
+                do
                 {
-                    Console.WriteLine("Kids deal: 80kr");
+                    Console.WriteLine("Please enter the age of the movie goer (numericaly):");
+                    input = Console.ReadLine();
+                } while (!int.TryParse(input, out movieGoerAge));
+
+                if (movieGoerAge < 20 || movieGoerAge > 64)
+                {
+                    if (movieGoerAge == 0 || movieGoerAge >= 100)
+                    {
+                        groupPrice = groupPrice + 0;
+                    }
+                    else if (movieGoerAge < 20)
+                    {
+                        groupPrice += 80;
+                    }
+                    else
+                    {
+                        groupPrice += 90;
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Senior citizen discount: 90kr");
+                    groupPrice += 120;
                 }
             }
-            else
-            {
-                Console.WriteLine("Standard Price: 120kr");
-            }
-
+            Console.WriteLine("The total comes to: " + groupPrice);
             #region ex
+            //if (movieGoerAge < 20 || movieGoerAge > 64)
+            //{
+            //    if (movieGoerAge == 0 || movieGoerAge >= 100)
+            //    {
+            //        Console.WriteLine("Congratulations, free ticket!");
+            //    }
+            //    else if (movieGoerAge < 20)
+            //    {
+            //        Console.WriteLine("Kids deal: 80kr");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Senior citizen discount: 90kr");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Standard Price: 120kr");
+            //}
+
             //if (movieGoerAge < 20)
             //{
             //    Console.WriteLine("Kids deal: 80kr");
@@ -79,15 +124,29 @@ namespace CodeAlong180309
         {
             Console.WriteLine("Just write like whatever:");
             string input = Console.ReadLine();
-            for(int i=1; i<=10;i++)
+            for (int i = 1; i <= 10; i++)
             {
-                Console.Write(i+"."+input+", ");
+                Console.Write(i + "." + input + ", ");
             }
         }
 
         public static void ThirdWord()
         {
+            while (true)
+            {
+                Console.WriteLine("Please enter a sentence with at least three words");
+                string input = Console.ReadLine();
+                string[] theWords =
+                    input
+                    .Split(new char[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries);
 
+                if (theWords.Length >= 3)
+                {
+                    Console.WriteLine(theWords[2]);
+                    break;
+                }
+            }
         }
     }
 }
